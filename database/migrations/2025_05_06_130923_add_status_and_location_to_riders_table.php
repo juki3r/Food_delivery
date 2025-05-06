6531cc1f3a182b61b94326e6bb5bef0d2e3daaca
juki3r <deepong25@gmail.com>
2025-05-06 21:12:07 +0800

    new file:   database/migrations/2025_05_06_130923_add_status_and_location_to_riders_table.php

---
+++ database/migrations/2025_05_06_130923_add_status_and_location_to_riders_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('riders', function (Blueprint $table) {
+            $table->enum('status', ['online', 'offline'])->default('offline');
+            $table->string('current_location')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('riders', function (Blueprint $table) {
+            $table->dropColumn(['status', 'current_location']);
+        });
+    }
+};
